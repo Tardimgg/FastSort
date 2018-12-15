c003e8d8902b7340f14d943ef359c40328107e76
Tardimgg <44931062+Tardimgg@users.noreply.github.com>
2018-12-16 00:27:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,92 @@
+class FastSort {
+
+    public static void sort(int[] array, Integer size, Integer max) {
+        int length = size != null ? size : array.length;
+        if (max != null && max <= 110) {
+            ShellSort(array);
+        } else if (length <= 500000) {
+            MergeSort(array, 0, length - 1);
+        } else {
+            HeapSort(array);
+        }
+    }
+
+    private static void ShellSort(int[] array) {
+        int j;
+        for (int gap = array.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < array.length; i++) {
+                int temp = array[i];
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+        }
+    }
+
+    private static void HeapSort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--)
+            shiftDown(array, i, array.length);
+        for (int i = array.length - 1; i > 0; i--) {
+            swap(array, 0, i);
+            shiftDown(array, 0, i);
+        }
+    }
+
+    private static void shiftDown(int[] array, int i, int n) {
+        int child;
+        int tmp;
+
+        for (tmp = array[i]; leftChild(i) < n; i = child) {
+            child = leftChild(i);
+            if (child != n - 1 && (array[child] < array[child + 1]))
+                child++;
+            if (tmp < array[child])
+                array[i] = array[child];
+            else
+                break;
+        }
+        array[i] = tmp;
+    }
+
+    private static int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static void MergeSort(int[] array, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            MergeSort(array, low, mid);
+            MergeSort(array, mid + 1, high);
+            merge(array, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] array, int low, int mid, int high) {
+
+        int n = high - low + 1;
+        int[] Temp = new int[n];
+
+        int i = low, j = mid + 1;
+        int k = 0;
+
+        while (i <= mid || j <= high) {
+            if (i > mid)
+                Temp[k++] = array[j++];
+            else if (j > high)
+                Temp[k++] = array[i++];
+            else if (array[i] < array[j])
+                Temp[k++] = array[i++];
+            else
+                Temp[k++] = array[j++];
+        }
+        for (j = 0; j < n; j++)
+            array[low + j] = Temp[j];
+    }
+}
